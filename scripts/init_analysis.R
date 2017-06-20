3d175d62f9361ab19de2e27864c6452b9ad4f5f9
Alasdair Clarke <a.clarke@essex.ac.uk>
2017-06-20 16:37:36 +0100

    Getting everything organised slowly

--- scripts/init_analysis.R
+++ scripts/init_analysis.R
@@ -1,32 +1,61 @@
 library(ggplot2)
-
 library(dplyr)
 
 source("myFunctions.R")
 
 
-dat <- read.csv("../data/l19a_t1_samples.txt", sep="\t")
+alg = 'eyelink'
 
-# turn dataframe into list!
-saccades <- SaccDFtoList(dat, 'eyelink')
+trials = 1:10
 
-saccStats <- sapply(saccades, GetAmpAndDir)
 
+saccades  <- vector('list', length(trials))
+durations <- vector('list', length(trials))
+
+for (trl in trials)
+{
+	# read in trial data
+	dat <- read.csv(paste("../data/l19a_t", trl, "_samples.txt", sep=""), sep="\t")
+	# turn dataframe into list!
+	saccades[[trl]]  <- SaccDFtoList(dat, trl, alg)
+	# get fixation duration time
+	durations[[trl]] <- GetFixDur(dat)
+}
+
+# flatten the list-of-lists into a list
+saccades  <- unlist(saccades, recursive=FALSE)
+durations <- unlist(durations)
+
+# get basic saccade statistics
+saccStats <- sapply(saccades, GetSaccadeStatistics)
+saccStats <- as.data.frame(t(saccStats))
+saccStats$fixDur = durations
+
+# normalise saccades 
 saccadesNormalised = lapply(saccades, NormaliseSaccade)
 names(saccadesNormalised) <- 1:length(saccadesNormalised)
-
-
-
 sacDF <- do.call(rbind.data.frame, saccadesNormalised)
+
+#get curvature statistics
+curve <-sapply(saccadesNormalised, FUN=CurvatureStats, USE.NAMES = FALSE)
+
+
+curveDF <- cbind(as.data.frame((t(curve))), saccStats)
+
+for (cc in 1:length(curveDF))
+{
+	curveDF[,cc] <- as.numeric(curveDF[,cc])
+}
+
+
 plt <- ggplot(sacDF, aes(x=X, y=Y)) + geom_point()
-plt <- plt + facet_wrap(~saccNumber, scale="free")
-ggsave("../plots/sacTrace_Eyelink.pdf")
+plt <- plt + geom_text(data=curveDF, aes(x=0,y=0, label=round(curveDF$quad_R2,2)))
+plt <- plt + geom_smooth(method='lm', formula=y~x+I(x^2))
+plt <- plt + facet_wrap(trlNumber~saccNumber, scale="free")
+ggsave(paste("../plots/sacTrace_", alg, ".pdf", sep=""),width=20, height=20)
 
 
-
-
-curve <-sapply(saccadesNormalised, CurvatureStats, simplify='array')
-
-
-plot(saccStats[1,], curve[1,])
-
+plt <- ggplot(curveDF, aes(x=fixDur, y=quad_curvature))
+plt <- plt + geom_point()
+plt <- plt + geom_smooth()
+plt
--- scripts/myFunctions.R
+++ scripts/myFunctions.R
@@ -1,29 +1,74 @@
-SaccDFtoList <- function(saccDF, alg='eyelink')
+SaccDFtoList <- function(saccDF, trl, alg='eyelink')
 {
 	# take samples and split into a list of saccades
 
 	if (alg=='eyelink'){
 		sacBoundaries  <- which(saccDF$SacFlagEyeLink[1:(nrow(saccDF)-1)]!=saccDF$SacFlagEyeLink[2:nrow(saccDF)])
+	} else {
+		sacBoundaries  <- which(saccDF$SacFlagNystrom[1:(nrow(saccDF)-1)]!=saccDF$SacFlagNystrom[2:nrow(saccDF)])
 	}
 
+	# if trial ends during a saccade, remove that saccade
+	if (length(sacBoundaries) %% 2)
+	{
+		sacBoundaries <- sacBoundaries[-length(sacBoundaries)]
+	}
+
+	# now get extract saccade
 	saccDF <- select(saccDF, SampleTime, X, Y)
 	sacList <- vector('list', length(sacBoundaries)/2)
 
 	saccDF$saccNumber <- 0
+	saccDF$trlNumber  <- 0
 	saccCtr <- 0
+
 	for (ii in seq(1, length(sacBoundaries), 2))
 	{
 		saccCtr <- saccCtr + 1
-		idx = (sacBoundaries[ii]+1):sacBoundaries[ii+1]
-		saccDF$saccNumber[idx] = saccCtr
+		idx <- (sacBoundaries[ii]+1):sacBoundaries[ii+1]
+		saccDF$saccNumber[idx] <- saccCtr
+		saccDF$trlNumber[idx]  <- trl
 		sacList[[saccCtr]] <- rbind(saccDF[(sacBoundaries[ii]+1):sacBoundaries[ii+1],])
 		row.names(sacList[[saccCtr]]) = NULL
 	}
-	names(sacList) <- 1:length(sacList)
+
+	names(sacList) <- paste("trl", trl, "-sacc", 1:length(sacList), sep="")
 	return(sacList)
 }
 
-GetAmpAndDir <- function(saccade)
+GetFixDur <- function(saccDF, trl, alg='eyelink')
+{
+	# take samples and split into a list of saccades
+
+	if (alg=='eyelink'){
+		sacBoundaries  <- which(saccDF$SacFlagEyeLink[1:(nrow(saccDF)-1)]!=saccDF$SacFlagEyeLink[2:nrow(saccDF)])
+	} else {
+		sacBoundaries  <- which(saccDF$SacFlagNystrom[1:(nrow(saccDF)-1)]!=saccDF$SacFlagNystrom[2:nrow(saccDF)])
+
+	}
+
+	# if trial ends during a saccade, remove that saccade
+	if (length(sacBoundaries) %% 2)
+	{
+		sacBoundaries <- sacBoundaries[-length(sacBoundaries)]
+	}
+
+	# first, get preceeding fixation duration
+	fixDur <- vector('numeric', length(sacBoundaries)/2)
+	fixCtr <- 1
+	fixDur[1] = NaN # first fixation duration is unreliable
+	for (ii in seq(1, length(sacBoundaries)-2, 2))
+	{
+		fixCtr <- fixCtr + 1
+		idx <- (sacBoundaries[ii+1]+1):sacBoundaries[ii+2]
+
+		fixDur[fixCtr] <- saccDF$SampleTime[idx[length(idx)]] - saccDF$SampleTime[idx[1]]
+	}
+
+	return(fixDur)
+}
+
+GetSaccadeStatistics <- function(saccade)
 {
 	x1 <- saccade$X[1]
 	x2 <- saccade$X[nrow(saccade)]
@@ -31,21 +76,23 @@ GetAmpAndDir <- function(saccade)
 	y1 <- saccade$Y[1]
 	y2 <- saccade$Y[nrow(saccade)]
 
+	t1 <- saccade$SampleTime[1]
+	t2 <- saccade$SampleTime[nrow(saccade)]
+
 	r = sqrt((x2-x1)^2+(y2-y1)^2)
 
 	theta = atan2(y2-y1, x2-x1)
 
-	return(list(r=r, theta=theta))
-
-
+	nSamples = nrow(saccade)
+	saccDur = t2 - t1
+	return(list(x1=x1, y1=y1, r=r, theta=theta, nSample=nSamples, saccDur=saccDur))
 }
 
 NormaliseSaccade <- function(saccade)
 {
 	# first set saccade start point at (0, 0)
 	x <- saccade$X - saccade$X[1]
-	y <- saccade$ Y-saccade$Y[1]
-	
+	y <- saccade$Y -saccade$Y[1]
 
 	# now rotate so endpoint lands on (x, 0)
 	theta = atan2(y[nrow(saccade)], x[nrow(saccade)])
@@ -53,9 +100,14 @@ NormaliseSaccade <- function(saccade)
 	yr <- sin(-theta) * x + cos(-theta) * y
 
 	# now normalise so endpoint lands on (1, 0)
-	xn <- xr/xr[length(xr)]
+	xn <- 2 * (xr/xr[length(xr)]) - 1
 	yn <- yr/xr[length(xr)]
 
+	# now reflect is -ve curvature
+	if (abs(min(yn))>max(yn))
+	{
+		yn <- -yn
+	}
 	# output!!
 	saccade[,2:3] = cbind(xn, yn)
 	return(saccade)
@@ -70,11 +122,15 @@ CurvatureStats <- function(saccade)
 
 	# quadratic
 	qfit   <- lm(Y~X+I(X^2), saccade)
-	quad_c <- abs(qfit$coefficients[3])
+	quad_c <- as.numeric(abs(qfit$coefficients[3]))
+	quad_R2<- as.numeric(summary(qfit)$r.squared)
 
-
-	return(list(
+	return(data.frame(
+		trlNumber=saccade$trlNumber[1],
+		saccNumber=saccade$saccNumber[1],
 		max_curvature=max_c, 
 		area_curvature=area_c,
-		quad_curvature=quad_c)) 
+		quad_curvature=quad_c,
+		quad_R2 = quad_R2
+		)) 
 }
